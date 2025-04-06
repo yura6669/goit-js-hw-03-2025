c72719b94da1bf7d582d33b78ef218ad77740ab9
yura6669 <yurii.shyian.01@gmail.com>
2025-04-06 14:44:12 +0300

    Implement tasks

---
+++ js/task-2.js
@@ -0,0 +1,17 @@
+console.log('------------------- Task2 -------------------');
+
+function makeArray(firstArray, secondArray, maxLength) {
+    const resultArray = firstArray.concat(secondArray);
+    if (resultArray.length > maxLength) { 
+        return resultArray.slice(0, maxLength);
+    } else {
+        return resultArray;
+    }
+}
+
+console.log(makeArray(["Mango", "Poly"], ["Ajax", "Chelsea"], 3)); // ["Mango", "Poly", "Ajax"]
+console.log(makeArray(["Mango", "Poly", "Houston"], ["Ajax", "Chelsea"], 4)); // ["Mango", "Poly", "Houston", "Ajax"]
+console.log(makeArray(["Mango"], ["Ajax", "Chelsea", "Poly", "Houston"], 3)); // ["Mango", "Ajax", "Chelsea"]
+console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 2)); // ["Earth", "Jupiter"]
+console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 4)); // ["Earth", "Jupiter", "Neptune", "Uranus"]
+console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus", "Venus"], 0)); // []
